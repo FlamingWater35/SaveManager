8abc1cbeee3008202828f2ab3f1530587016609c
FlamingWater35 <flamingwater69420@gmail.com>
2025-02-20 13:35:32 +0200

    Update compiling script

--- compile.ps1
+++ compile.ps1
@@ -14,4 +14,6 @@ nuitka --standalone --windows-console-mode=disable --file-version=2.6.0.0 --prod
 
 #nuitka --onefile --standalone --windows-console-mode=disable --file-version=2.6.0.0 --product-version=2.6.0.0 --file-description="Save Manager" --product-name="Save Manager" --copyright="© 2025 Flaming Water" --windows-icon-from-ico="C:\Users\Admin\Documents\work\projects\VSCodeProjects\SaveManager\docs\icon.ico" --include-module=win32gui --include-module=win32api --include-data-dir="C:\Users\Admin\Documents\work\projects\VSCodeProjects\SaveManager\docs=docs" --include-data-files="C:\Users\Admin\Documents\work\projects\VSCodeProjects\SaveManager\docs\openh264-1.8.0-win64.dll=docs/openh264-1.8.0-win64.dll" --enable-plugin=tk-inter --include-data-dir="C:\Users\Admin\Documents\work\projects\VSCodeProjects\SaveManager\.conda\Lib\site-packages\customtkinter=customtkinter" --output-dir="C:\Users\Admin\Documents\work\projects\VSCodeProjects\SaveManager\main" --enable-plugin=upx --upx-binary="C:\Users\Admin\Documents\work\projects\upx-4.2.4-win64\upx-4.2.4-win64" --lto=yes --clang --remove-output SaveManager_onefile.py
 
+python create_archive.py
+
 Start-Sleep -Seconds 3
